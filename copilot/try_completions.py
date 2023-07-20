a03b947d4d12531a65e69e023b996f01ccbf692c
Oleksandr Tereshchenko <toporok@gmail.com>
2023-07-20 09:43:02 +0300

    chat_completion_for_repo_file

--- copilot/try_completions.py
+++ copilot/try_completions.py
@@ -3,21 +3,13 @@ from pathlib import Path
 from copilot.cached_completions import RepoCompletions
 from copilot.utils import FAST_GPT_MODEL
 
-
 repo_completions = RepoCompletions(
     repo=Path(__file__).parent,
     completion_name="helloworld",
+    model=FAST_GPT_MODEL,
 )
 
 
 async def main() -> None:
     messages = [{"role": "assistant", "content": "Привіт world!"}]
-    print(
-        await repo_completions.chat_completion_for_file(
-            messages=messages,
-            repo=Path(__file__).parent,
-            repo_file=Path(__file__),
-            completion_name="helloworld",
-            model=FAST_GPT_MODEL,
-        )
-    )
+    print(await repo_completions.chat_completion_for_file(messages=messages, repo_file=Path(__file__)))
