62e2a7a75c2e95401485dbd320e02bcd52bf5415
Oleksandr Tereshchenko <toporok@gmail.com>
2023-07-22 11:13:27 +0300

    explain a repo

--- copilot/try_completions.py
+++ copilot/try_completions.py
@@ -1,16 +1,11 @@
 from pathlib import Path
+from pprint import pprint
 
 from langchain.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
 
 from copilot.cached_completions import RepoCompletions
 from copilot.repo_access_utils import list_files_in_repo
-from copilot.utils import FAST_GPT_MODEL
-
-repo_completions = RepoCompletions(
-    repo=Path(__file__).parents[1],
-    completion_name="helloworld",
-    model=FAST_GPT_MODEL,
-)
+from copilot.utils import FAST_GPT_MODEL, FAST_LONG_GPT_MODEL, SLOW_GPT_MODEL, convert_lc_message_to_openai
 
 EXPLAIN_FILE_PROMPT = ChatPromptTemplate.from_messages(
     [
@@ -19,6 +14,23 @@ EXPLAIN_FILE_PROMPT = ChatPromptTemplate.from_messages(
         SystemMessagePromptTemplate.from_template("Please explain the content of this file in plain English."),
     ]
 )
+REPO_PATH = Path(__file__).parents[2] / "Auto-GPT"
+
+gpt3_explainer = RepoCompletions(
+    repo=REPO_PATH,
+    completion_name="gpt3-expl",
+    model=FAST_GPT_MODEL,
+)
+gpt3_long_explainer = RepoCompletions(
+    repo=REPO_PATH,
+    completion_name="gpt3-long-expl",
+    model=FAST_LONG_GPT_MODEL,
+)
+gpt4_explainer = RepoCompletions(
+    repo=REPO_PATH,
+    completion_name="gpt4-expl",
+    model=SLOW_GPT_MODEL,
+)
 
 
 async def main() -> None:
@@ -26,9 +38,7 @@ async def main() -> None:
     # print(await repo_completions.chat_completion_for_file(messages=messages, repo_file=Path(__file__)))
 
     repo_files = [
-        f
-        for f in list_files_in_repo(Path(__file__).parents[2] / "Auto-GPT", additional_gitignore_content="tests/")
-        if f.suffix.lower() == ".py"
+        f for f in list_files_in_repo(REPO_PATH, additional_gitignore_content="tests/") if f.suffix.lower() == ".py"
     ]
     print()
     for file in repo_files:
@@ -36,3 +46,11 @@ async def main() -> None:
     print()
     print(len(repo_files))
     print()
+
+    file = "autogpt/core/planning/simple.py"
+    prompt_messages = EXPLAIN_FILE_PROMPT.format_messages(
+        file_path=file,
+        file_content=(REPO_PATH / file).read_text(encoding="utf-8"),
+    )
+    prompt_messages = [convert_lc_message_to_openai(pm) for pm in prompt_messages]
+    pprint(prompt_messages, sort_dicts=False)
--- copilot/utils.py
+++ copilot/utils.py
@@ -1,3 +1,17 @@
+from langchain.schema import BaseMessage
+
 FAST_GPT_MODEL = "gpt-3.5-turbo-0613"
 FAST_LONG_GPT_MODEL = "gpt-3.5-turbo-16k-0613"
 SLOW_GPT_MODEL = "gpt-4-0613"
+
+
+def convert_lc_message_to_openai(message: BaseMessage) -> dict[str, str]:
+    if message.type == "human":
+        role = "user"
+    elif message.type == "ai":
+        role = "assistant"
+    elif message.type == "system":
+        role = "system"
+    else:
+        raise ValueError(f"Unexpected message type: {message.type}")
+    return {"role": role, "content": message.content}
