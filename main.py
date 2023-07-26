36f72436ebe636b8b7f50f31559b80963e4bb951
Oleksandr Tereshchenko <toporok@gmail.com>
2023-07-26 09:12:24 +0300

    explain-langchain

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# from copilot.explain_full_repo import main
-from copilot.try_completion import main
+# from copilot.try_completion import main
+from copilot.explain_full_repo import main
 
 if __name__ == "__main__":
     asyncio.run(main())
