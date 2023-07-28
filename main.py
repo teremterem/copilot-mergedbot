edc8f93538423ce50aeb1f253996c01daf1ea6cb
Oleksandr Tereshchenko <toporok@gmail.com>
2023-07-28 20:52:54 +0300

    embed-langchain

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# from copilot.explain_repo import embed_everything
-from copilot.try_completion import main
+# from copilot.try_completion import main
+from copilot.explain_repo import embed_everything
 
 if __name__ == "__main__":
-    asyncio.run(main())
-    # asyncio.run(embed_everything())
+    # asyncio.run(main())
+    asyncio.run(embed_everything())
