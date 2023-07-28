bbffd2c78eea7ac095f54a620ca30dcc8feb29ca
Oleksandr Tereshchenko <toporok@gmail.com>
2023-07-28 18:40:42 +0300

    embed-langchain

--- copilot/try_completion.py
+++ copilot/try_completion.py
@@ -1,17 +1,17 @@
 from copilot.explain_repo import explain_repo_file_in_isolation
-from copilot.specific_repo import list_files_in_specific_repo_chunked
 
 
 async def main() -> None:
-    repo_files = list_files_in_specific_repo_chunked(reduced_list=True)[0]
-    print()
-    for file in repo_files:
-        print(file)
-    print()
-    print(len(repo_files))
-    print()
+    # repo_files = list_files_in_specific_repo_chunked(reduced_list=True)[0]
+    # print()
+    # for file in repo_files:
+    #     print(file)
+    # print()
+    # print(len(repo_files))
+    # print()
 
     file = "libs/experimental/langchain_experimental/cpal/base.py"
     explanation = await explain_repo_file_in_isolation(file)
+    print()
     print(explanation)
     print()
