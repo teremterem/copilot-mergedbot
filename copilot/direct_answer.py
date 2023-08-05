451ffc30a2593977a0713b855a4908c04f609657
Oleksandr Tereshchenko <toporok@gmail.com>
2023-08-05 16:52:13 +0300

    more-subbots

--- copilot/direct_answer.py
+++ copilot/direct_answer.py
@@ -1,14 +1,12 @@
 # pylint: disable=no-name-in-module
 import itertools
 
-from botmerger import SingleTurnContext
+from botmerger import SingleTurnContext, BotResponses
 from langchain.prompts import ChatPromptTemplate, SystemMessagePromptTemplate
-from langchain.schema import HumanMessage
 
 from copilot.chat_history_filter import chat_history_filter
-from copilot.code_extractors import extract_relevant_snippets
+from copilot.code_extractors import extract_snippets
 from copilot.relevant_files import relevant_files
-from copilot.request_condenser import request_condenser
 from copilot.specific_repo import REPO_PATH_IN_QUESTION
 from copilot.utils.misc import (
     SLOW_GPT_MODEL,
@@ -48,20 +46,22 @@ paths).\
 @bot_merger.create_bot("DirectAnswerBot")
 async def direct_answer(context: SingleTurnContext) -> None:
     relevant_file_messages = await relevant_files.bot.get_all_responses(context.concluding_request)
+
     recalled_files_msg = "\n".join(file_msg.extra_fields["file"] for file_msg in relevant_file_messages)
     await context.yield_interim_response(f"```\n{recalled_files_msg}\n```", invisible_to_bots=True)
 
-    prompt_prefix = DIRECT_ANSWER_PROMPT_PREFIX.format_messages(repo_name=REPO_PATH_IN_QUESTION.name)
-    standalone_request = await request_condenser.bot.get_final_response(context.concluding_request)
-    recalled_files = [
-        # TODO run code snippet extractors in parallel
-        HumanMessage(
-            content=await extract_relevant_snippets(file_msg.extra_fields["file"], standalone_request.content)
+    promises: list[BotResponses] = []
+    for file_msg in relevant_file_messages:
+        promises.append(
+            await extract_snippets.trigger(
+                context.concluding_request, extra_fields={"file": file_msg.extra_fields["file"]}
+            )
         )
-        for file_msg in relevant_file_messages
-    ]
+
+    prompt_prefix = DIRECT_ANSWER_PROMPT_PREFIX.format_messages(repo_name=REPO_PATH_IN_QUESTION.name)
+    recalled_snippets = [await promise.get_final_response() for promise in promises]
     prompt_suffix = DIRECT_ANSWER_PROMPT_SUFFIX.format_messages()
-    prompt_openai = langchain_messages_to_openai(itertools.chain(prompt_prefix, recalled_files, prompt_suffix))
+    prompt_openai = langchain_messages_to_openai(itertools.chain(prompt_prefix, recalled_snippets, prompt_suffix))
 
     conversation = await chat_history_filter.bot.get_all_responses(context.concluding_request)
     prompt_openai.extend(
