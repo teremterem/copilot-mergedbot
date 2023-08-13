467a7b0c1ffa7bf39b73654a45ea78cfeb82ab9c
Oleksandr Tereshchenko <toporok@gmail.com>
2023-08-13 18:26:39 +0300

    rename invisible_to_bots to hidden_from_history

--- copilot/direct_answer.py
+++ copilot/direct_answer.py
@@ -49,7 +49,7 @@ async def direct_answer(context: SingleTurnContext) -> None:
     relevant_file_messages = await relevant_files.bot.get_all_responses(context.concluding_request)
 
     recalled_files_msg = "\n".join(file_msg.extra_fields["file"] for file_msg in relevant_file_messages)
-    await context.yield_interim_response(f"```\n{recalled_files_msg}\n```", invisible_to_bots=True)
+    await context.yield_interim_response(f"```\n{recalled_files_msg}\n```", hidden_from_history=True)
 
     promises: list[BotResponses] = []
     for file_msg in relevant_file_messages:
