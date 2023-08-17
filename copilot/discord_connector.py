4be4db9afaa7587b545b736eb10387577f2530aa
Oleksandr Tereshchenko <toporok@gmail.com>
2023-08-17 21:27:01 +0300

    yaml-replay

--- copilot/discord_connector.py
+++ copilot/discord_connector.py
@@ -1,11 +1,14 @@
 # pylint: disable=wrong-import-position
 """Main file for running the Discord bot."""
 import os
+from uuid import UUID
 
 import discord
+from botmerger import SingleTurnContext
 from botmerger.ext.discord_integration import attach_bot_to_discord
 
 from copilot.direct_answer import main_bot
+from copilot.utils.misc import bot_merger
 
 DISCORD_BOT_SECRET = os.environ["DISCORD_BOT_SECRET"]
 
@@ -22,3 +25,13 @@ async def on_ready() -> None:
 def main() -> None:
     attach_bot_to_discord(main_bot, discord_client)
     discord_client.run(DISCORD_BOT_SECRET)
+
+
+def replay() -> None:
+    attach_bot_to_discord(replay_bot.bot, discord_client)
+    discord_client.run(DISCORD_BOT_SECRET)
+
+
+@bot_merger.create_bot
+async def replay_bot(context: SingleTurnContext) -> None:
+    await context.yield_from(await context.merger.replay(UUID(context.concluding_request.content)))
